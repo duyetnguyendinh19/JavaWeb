fcd8c85109576e3f711722b26956023e18a0dd20
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-07-11 11:52:31 +0700

    Create HibernateUtils.java

---
+++ src/main/java/shop/newshop/util/HibernateUtils.java
@@ -0,0 +1,23 @@
+package shop.newshop.util;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+@SuppressWarnings("deprecation")
+public class HibernateUtils {
+	private static final SessionFactory sessionFactory;
+
+	static {
+		try {
+			sessionFactory = new Configuration().configure("Hibernate.cfg.xml").buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
