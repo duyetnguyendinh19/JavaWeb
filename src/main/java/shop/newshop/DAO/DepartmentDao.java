7154a4168d745a6660296ad9f6b9603a42f619b3
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-07-16 17:54:46 +0700

    Update DepartmentDao.java

--- src/main/java/shop/newshop/DAO/DepartmentDao.java
+++ src/main/java/shop/newshop/DAO/DepartmentDao.java
@@ -1,5 +1,19 @@
 package shop.newshop.DAO;
 
-public class DepartmentDao {
+import java.util.List;
 
+import shop.newshop.Entity.Department;
+
+public interface DepartmentDao {
+	
+	public List<Department> getAlls();
+	
+	public boolean insert(Department de);
+	
+	public boolean update(Department de);
+	
+	public boolean delete(int idDepart);
+	
+	public Department getDepartById(int idDepart);
+	
 }
