fc2af64df6670d2d929c2f83fa51f959a8178f44
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-07-16 17:54:49 +0700

    Create DisciplineDao.java

---
+++ src/main/java/shop/newshop/DAO/DisciplineDao.java
@@ -0,0 +1,17 @@
+package shop.newshop.DAO;
+
+import java.util.List;
+
+import shop.newshop.Entity.Discipline;
+
+public interface DisciplineDao {
+	public List<Discipline> getAlls();
+
+	public boolean insert(Discipline di);
+
+	public boolean update(Discipline di);
+
+	public boolean delete(int idDiscipline);
+
+	public Discipline getDisciplineById(int idDiscipline);
+}
