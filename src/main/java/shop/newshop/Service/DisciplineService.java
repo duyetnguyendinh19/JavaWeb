c8180affeff45dfd7eb8b4de77dda3248f585023
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-07-16 17:55:15 +0700

    Create DisciplineService.java

---
+++ src/main/java/shop/newshop/Service/DisciplineService.java
@@ -0,0 +1,17 @@
+package shop.newshop.Service;
+
+import java.util.List;
+
+import shop.newshop.Entity.Discipline;
+
+public interface DisciplineService {
+	public List<Discipline> getAlls();
+
+	public boolean insert(Discipline di);
+
+	public boolean update(Discipline di);
+
+	public boolean delete(int idDiscipline);
+
+	public Discipline getDisciplineById(int idDiscipline);
+}
