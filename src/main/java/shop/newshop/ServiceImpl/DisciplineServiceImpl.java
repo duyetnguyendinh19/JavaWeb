9161362308578145bcaf3412ddd001b45ec1601f
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-07-16 17:55:27 +0700

    Create DisciplineServiceImpl.java

---
+++ src/main/java/shop/newshop/ServiceImpl/DisciplineServiceImpl.java
@@ -0,0 +1,48 @@
+package shop.newshop.ServiceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import shop.newshop.DAO.DisciplineDao;
+import shop.newshop.Entity.Discipline;
+import shop.newshop.Service.DisciplineService;
+
+@Service
+public class DisciplineServiceImpl implements DisciplineService{
+	
+	@Autowired
+	private DisciplineDao disDao;
+	
+	@Override
+	public List<Discipline> getAlls() {
+		
+		return disDao.getAlls();
+	}
+
+	@Override
+	public boolean insert(Discipline di) {
+		
+		return disDao.insert(di);
+	}
+
+	@Override
+	public boolean update(Discipline di) {
+		
+		return disDao.update(di);
+	}
+
+	@Override
+	public boolean delete(int idDiscipline) {
+		
+		return disDao.delete(idDiscipline);
+	}
+
+	@Override
+	public Discipline getDisciplineById(int idDiscipline) {
+		
+		return disDao.getDisciplineById(idDiscipline);
+	}
+
+}
