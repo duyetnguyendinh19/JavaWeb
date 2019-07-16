c79eaa385a23aede365578e05667708cd2dd0ef5
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-07-16 17:55:09 +0700

    Create ContractService.java

---
+++ src/main/java/shop/newshop/Service/ContractService.java
@@ -0,0 +1,19 @@
+package shop.newshop.Service;
+
+import java.util.List;
+
+import shop.newshop.Entity.Contract;
+
+public interface ContractService {
+
+	public List<Contract> getAlls();
+
+	public boolean insert(Contract ct);
+
+	public boolean update(Contract ct);
+
+	public boolean delete(int idContract);
+
+	public Contract getContractById(int idContract);
+
+}
