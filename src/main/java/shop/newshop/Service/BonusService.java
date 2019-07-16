cc31320f3e8b094b1b171a847371133d8996f736
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-07-16 17:55:07 +0700

    Create BonusService.java

---
+++ src/main/java/shop/newshop/Service/BonusService.java
@@ -0,0 +1,19 @@
+package shop.newshop.Service;
+
+import java.util.List;
+
+import shop.newshop.Entity.Bonus;
+
+public interface BonusService {
+
+	public List<Bonus> getAlls();
+
+	public boolean insert(Bonus bn);
+
+	public boolean update(Bonus bn);
+
+	public boolean delete(int idBonus);
+
+	public Bonus getBonusById(int idBonus);
+
+}
