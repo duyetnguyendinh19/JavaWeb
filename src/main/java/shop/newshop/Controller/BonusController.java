ae5286efd734cac4a0d5b9522670af86d95b0449
Your Name <you@example.com>
2019-07-17 21:12:43 +0700

    update AutoGenDB

--- src/main/java/shop/newshop/Controller/BonusController.java
+++ src/main/java/shop/newshop/Controller/BonusController.java
@@ -5,18 +5,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import shop.newshop.Service.BonusService;
+import shop.newshop.Entity.*;
+import shop.newshop.Service.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class BonusController {
 
 	@Autowired
 	private BonusService bonusService;
+
+	@Autowired
+	private ContractService contractService;
+
+	@Autowired
+	private DepartmentService departmentService;
+
+	@Autowired
+	private DisciplineService disciplineService;
+
+	@Autowired
+	private EmployeeService employeeService;
 	
 	@GetMapping(value = "listBonus")
 	public String getBonus(ModelMap map) {
+		List<Employee> listContract = new ArrayList<>();
+		listContract =  employeeService.getAlls();
+		for(Employee x : listContract){
+			System.out.println("Contract: " + x.getPhone());
+		}
 		map.put("lstBonus", bonusService.getAlls());
-		return "";
+		return "admin/index";
 	}
 	
 }
