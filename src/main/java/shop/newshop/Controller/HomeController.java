16c18dcfba4142e1cc04de7facdc99d30b070bd6
Your Name <you@example.com>
2019-07-17 08:44:21 +0700

    loadProject

--- src/main/java/shop/newshop/Controller/HomeController.java
+++ src/main/java/shop/newshop/Controller/HomeController.java
@@ -1,11 +1,15 @@
 package shop.newshop.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+	@Autowired
+//	private
 	
 	@GetMapping(value="index")
 	public String HomeCustomer(ModelMap map) {
