af3bfaff0f1816b4a48ee92a20e429394c5730f5
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-07-17 08:42:35 +0700

    Update BonusDaoImpl.java

--- src/main/java/shop/newshop/DaoImpl/BonusDaoImpl.java
+++ src/main/java/shop/newshop/DaoImpl/BonusDaoImpl.java
@@ -2,18 +2,32 @@ package shop.newshop.DaoImpl;
 
 import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
 import shop.newshop.DAO.BonusDao;
 import shop.newshop.Entity.Bonus;
+import shop.newshop.util.HibernateUtils;
 
 @Repository
 public class BonusDaoImpl implements BonusDao{
 
 	@Override
 	public List<Bonus> getAlls() {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = HibernateUtils.getSessionFactory().getCurrentSession();
+		List<Bonus> lstBonus = null;
+//		try {
+//			session.beginTransaction();
+//			String hql = "FROM Bonus";
+//			Query query = session.createQuery(hql);
+//			lstBonus = query.list();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}finally{
+//			session.close();
+//		}
+		return lstBonus;
 	}
 
 	@Override
