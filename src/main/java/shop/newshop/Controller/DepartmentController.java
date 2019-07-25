cc31b45c59d27e2b5ee6685dd0fc9c71decc3aa4
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-07-25 09:29:01 +0700

    Update DepartmentController.java

--- src/main/java/shop/newshop/Controller/DepartmentController.java
+++ src/main/java/shop/newshop/Controller/DepartmentController.java
@@ -2,6 +2,8 @@ package shop.newshop.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import shop.newshop.Service.DepartmentService;
 
@@ -10,5 +12,10 @@ public class DepartmentController {
 
 	@Autowired
 	DepartmentService departService;
+	
+	@GetMapping(value = "listDepartment")
+	public String listDepartment(ModelMap model) {
+		return  "admin/listDepartment";
+	}
 	
 }
