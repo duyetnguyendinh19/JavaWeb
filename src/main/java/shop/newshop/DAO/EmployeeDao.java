42a78953aaa2dcbd9e6ff65f2e4349536eb42357
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-07-31 17:49:32 +0700

    Update EmployeeDao.java

--- src/main/java/shop/newshop/DAO/EmployeeDao.java
+++ src/main/java/shop/newshop/DAO/EmployeeDao.java
@@ -2,12 +2,13 @@ package shop.newshop.DAO;
 
 import java.util.List;
 
+import shop.newshop.Entity.Account;
 import shop.newshop.Entity.Employee;
 
 public interface EmployeeDao {
 	public List<Employee> getAlls();
 
-	public boolean insert(Employee emp);
+	public boolean insert(Employee emp,Account account);
 
 	public boolean update(Employee emp);
 
