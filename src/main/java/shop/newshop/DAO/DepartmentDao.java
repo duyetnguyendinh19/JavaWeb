88af22d621cf7286acb3aab5d1a11c06c22355fc
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-07-31 17:49:28 +0700

    Update DepartmentDao.java

--- src/main/java/shop/newshop/DAO/DepartmentDao.java
+++ src/main/java/shop/newshop/DAO/DepartmentDao.java
@@ -5,19 +5,21 @@ import java.util.List;
 import shop.newshop.Entity.Department;
 
 public interface DepartmentDao {
-	
+
 	public List<Department> getAlls(int startnum, int rownum);
-	
+
 	public boolean insert(Department de);
-	
+
 	public boolean update(Department de);
-	
+
 	public boolean delete(int idDepart);
-	
+
 	public Department getDepartById(int idDepart);
-	
+
+	public long checkName(String nameDepart, int idDepart);
+
 	public long countAll();
 
 	public List<Department> getAlls();
-	
+
 }
