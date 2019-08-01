24cfc99d4c5a85fdfff86e48ab10d3aadda149e1
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-08-01 10:22:54 +0700

    Update DepartmentController.java

--- src/main/java/shop/newshop/Controller/DepartmentController.java
+++ src/main/java/shop/newshop/Controller/DepartmentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 import shop.newshop.Entity.Department;
 import shop.newshop.Service.DepartmentService;
@@ -19,12 +19,15 @@ public class DepartmentController {
 
 	@Autowired
 	DepartmentService departService;
+	
+	private String name;
 
-	@GetMapping(value = "admin/listDepartment")
-	public String listDepartment(ModelMap model) {
-		model.put("listDepart", departService.getAlls(0,5));
-
-		long countAll = departService.countAll();
+	
+	@PostMapping(value = "admin/listDepartment")
+	public String listDepartmentSearch(ModelMap model,@RequestParam("nameDepart") String nameDepart) {
+		model.put("listDepart", departService.getAlls(0,5,nameDepart));
+		name =  nameDepart;
+		long countAll = departService.countAll(nameDepart);
 		long totalPage = 0;
 
 		if(countAll%5==0) {
@@ -34,14 +37,48 @@ public class DepartmentController {
 		}
 
 		model.put("totalPage", totalPage);
+		model.put("totalDepart", countAll);
+		model.put("firstDepart", 1);
+		if(countAll<5) {
+			model.put("lastDepart", countAll);
+		}else {
+			model.put("lastDepart", 5);
+		}
+		model.put("nameSearch", name);
+	
+		return  "admin/Department";
+	}
+	
+	@GetMapping(value = "admin/listDepartment")
+	public String listDepartment(ModelMap model) {
+		model.put("listDepart", departService.getAlls(0,5,null));
+		long countAll = departService.countAll(null);
+		long totalPage = 0;
+
+		if(countAll%5==0) {
+			totalPage = countAll/5;
+		}else {
+			totalPage = countAll/5 + 1;
+		}
+
+		model.put("totalPage", totalPage);
+		model.put("totalDepart", countAll);
+		model.put("firstDepart", 1);
+		if(countAll<5) {
+			model.put("lastDepart", countAll);
+		}else {
+			model.put("lastDepart", 5);
+		}
+		model.put("nameSearch", null);
+	
 		return  "admin/Department";
 	}
 
 	@GetMapping(value = "admin/listDepartment/{page}")
 	public String listDepartmentPage(ModelMap model, @PathVariable("page") int page) {
-		model.put("listDepart", departService.getAlls((page-1)*5,5));
+		model.put("listDepart", departService.getAlls((page-1)*5,5,name));
 
-		long countAll = departService.countAll();
+		long countAll = departService.countAll(name);
 		long totalPage = 0;
 
 		if(countAll%5==0) {
@@ -51,6 +88,15 @@ public class DepartmentController {
 		}
 
 		model.put("totalPage", totalPage);
+		model.put("totalDepart", countAll);
+		model.put("firstDepart", (page-1)*5+1);
+		if(page<totalPage) {
+			model.put("lastDepart", (page-1)*5+5);
+		}else {
+			model.put("lastDepart", countAll);
+		}
+		
+		model.put("nameSearch", name);
 		return  "admin/Department";
 	}
 
