f20234c5f65d4dcdaf7855e0f224c159fe22457f
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-08-02 14:56:53 +0700

    Update DisciplineController.java

--- src/main/java/shop/newshop/Controller/DisciplineController.java
+++ src/main/java/shop/newshop/Controller/DisciplineController.java
@@ -16,137 +16,190 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Controller
-@RequestMapping(value = "admin")
 public class DisciplineController {
 
-    @Autowired
-    private BonusService bonusService;
+	@Autowired
+	private DisciplineService disciplineService;
 
-    @Autowired
-    private ContractService contractService;
+	@Autowired
+	private EmployeeService employeeService;
 
-    @Autowired
-    private DepartmentService departmentService;
+	private String name;
 
-    @Autowired
-    private DisciplineService disciplineService;
+	@PostMapping(value = "admin/listDiscipline")
+	public String disciplineListSearch(ModelMap model, @RequestParam("nameEmployee") String nameSearch) {
+		name = nameSearch;
+		model.put("error", "");
+		model.put("disciplineList", disciplineService.getLimit(0, 5, nameSearch));
+		long countAll = disciplineService.countAll(nameSearch);
+		long totalPage = 0;
 
-    @Autowired
-    private EmployeeService employeeService;
+		if (countAll % 5 == 0) {
+			totalPage = countAll / 5;
+		} else {
+			totalPage = countAll / 5 + 1;
+		}
 
-    @RequestMapping(value = "listDiscipline", method = RequestMethod.GET)
-    public String getDiscipline(ModelMap map) {
-        List<Discipline> disciplineList = new ArrayList<>();
-        disciplineList = disciplineService.getAlls();
-        map.put("disciplineList", disciplineList);
-        return "admin/Discipline";
-    }
+		model.put("totalPage", totalPage);
+		model.put("totalDis", countAll);
+		model.put("firstDis", 1);
+		if (countAll < 5) {
+			model.put("lastDis", countAll);
+		} else {
+			model.put("lastDis", 5);
+		}
+		model.put("nameSearch", nameSearch);
 
-    @RequestMapping(value = "listDiscipline", method = RequestMethod.POST)
-    public String getBonusPost(ModelMap map , @RequestParam("tennv") String nameEmployee) {
-        List<Discipline> disciplineList = new ArrayList<>();
-        disciplineList = disciplineService.searchNameEmployee(nameEmployee);
-        if(disciplineList.size() == 0){
-            map.addAttribute("searchFail", "Không tìm thấy nhân viên là: " + nameEmployee);
-        }
-        map.addAttribute("nameSearch" ,nameEmployee);
-        map.put("disciplineList", disciplineList);
-        return "admin/Discipline";
-    }
+		return "admin/Discipline";
+	}
 
-    @GetMapping(value = "addDiscipline")
-    public String addContract(ModelMap model) {
-        List<Employee> employees = employeeService.getAlls();
-        model.addAttribute("employee", employees);
-        return "admin/AddDiscipline";
-    }
+	@GetMapping(value = "admin/listDiscipline")
+	public String disciplineList(ModelMap model) {
+		model.put("error", "");
+		model.put("disciplineList", disciplineService.getLimit(0, 5, null));
+		long countAll = disciplineService.countAll(null);
+		long totalPage = 0;
 
-    @PostMapping(value = "addDiscipline")
-    public String insertContract(RedirectAttributes redirectAttributes, ModelMap model, @RequestParam("typeDiscipline") String typeDiscipline,
-                                 @RequestParam("date") String date, @RequestParam("descent") String descent, @RequestParam("reason") String reason,
-                                 @RequestParam("employeeId") int idEmployee) {
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-            Employee employee = new Employee();
-            Discipline discipline = new Discipline();
-            if (Strings.isEmpty(date)) {
-                List<Employee> employees = employeeService.getAlls();
-                model.addAttribute("idEmployee", idEmployee);
-                model.addAttribute("employee", employees);
-                model.addAttribute("errorDate", "Ngày kỷ luật không được để trống");
-                return "admin/AddDiscipline";
-            }
-            if (Strings.isEmpty(reason)) {
-                List<Employee> employees = employeeService.getAlls();
-                model.addAttribute("idEmployee", idEmployee);
-                model.addAttribute("employee", employees);
-                model.addAttribute("errorReason", "Lý do kỷ luật không được để trống");
-                return "admin/AddDiscipline";
-            }
-            discipline.setDate(sdf.parse(date));
-            discipline.setDescent(descent);
-            discipline.setReason(reason);
-            employee.setId(idEmployee);
-            discipline.setEmployee(employee);
-            discipline.setType(typeDiscipline);
-            disciplineService.insert(discipline);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return "redirect:/admin/listDiscipline";
-    }
+		if (countAll % 5 == 0) {
+			totalPage = countAll / 5;
+		} else {
+			totalPage = countAll / 5 + 1;
+		}
 
-    @GetMapping(value = "updateDiscipline/{id}")
-    public String updateContract(ModelMap model, @PathVariable("id") int id) {
-        Discipline discipline = disciplineService.getDisciplineById(id);
-        model.addAttribute("discipline", discipline);
-        return "admin/EditDiscipline";
-    }
+		model.put("totalPage", totalPage);
+		model.put("totalDis", countAll);
+		model.put("firstDis", 1);
+		if (countAll < 5) {
+			model.put("lastDis", countAll);
+		} else {
+			model.put("lastDis", 5);
+		}
+		model.put("nameSearch", null);
 
-    @PostMapping(value = "updateDiscipline")
-    public String postUpdateContract(RedirectAttributes redirectAttributes, ModelMap model, @RequestParam("id") int id,
-                                     @RequestParam("typeDiscipline") String typeDiscipline, @RequestParam("date") String date,
-                                     @RequestParam("descent") String descent, @RequestParam("reason") String reason,
-                                     @RequestParam("employee") int idEmployee) throws ParseException {
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-            Employee employee = new Employee();
-            Discipline discipline = new Discipline();
-            if (Strings.isEmpty(date)) {
-                List<Employee> employees = employeeService.getAlls();
-                Discipline discipline1 = disciplineService.getDisciplineById(id);
-                discipline1.setDate(null);
-                model.addAttribute("discipline", discipline1);
-                model.addAttribute("employee", employees);
-                model.addAttribute("errorDate", "Ngày kỷ luật không được để trống");
-                return "admin/EditDiscipline";
-            }
-            if (Strings.isEmpty(reason)) {
-                List<Employee> employees = employeeService.getAlls();
-                Discipline discipline1 = disciplineService.getDisciplineById(id);
-                discipline1.setReason(null);
-                model.addAttribute("discipline", discipline1);
-                model.addAttribute("employee", employees);
-                model.addAttribute("errorReason", "Lý do kỷ luật không được để trống");
-                return "admin/EditDiscipline";
-            }
-            employee.setId(idEmployee);
-            discipline.setId(id);
-            discipline.setType(typeDiscipline);
-            discipline.setEmployee(employee);
-            discipline.setReason(reason);
-            discipline.setDescent(descent);
-            discipline.setDate(sdf.parse(date));
-            disciplineService.update(discipline);
-        } catch (ParseException pe) {
-            pe.printStackTrace();
-        }
-        return "redirect:/admin/listDiscipline";
-    }
+		return "admin/Discipline";
+	}
 
-    @GetMapping(value = "deleteDiscipline/{id}")
-    public String deleteDiscipline(ModelMap model, @PathVariable("id") int id) {
-        disciplineService.delete(id);
-        return "redirect:/admin/listDiscipline";
-    }
+	@GetMapping(value = "admin/listDiscipline/{page}")
+	public String disciplineListPage(ModelMap model, @PathVariable("page") int page) {
+		model.put("error", "");
+		model.put("disciplineList", disciplineService.getLimit((page - 1) * 5, 5, name));
+
+		long countAll = disciplineService.countAll(name);
+		long totalPage = 0;
+
+		if (countAll % 5 == 0) {
+			totalPage = countAll / 5;
+		} else {
+			totalPage = countAll / 5 + 1;
+		}
+
+		model.put("totalPage", totalPage);
+		model.put("totalDis", countAll);
+		model.put("firstDis", (page - 1) * 5 + 1);
+		if (page < totalPage) {
+			model.put("lastDis", (page - 1) * 5 + 5);
+		} else {
+			model.put("lastDis", countAll);
+		}
+
+		model.put("nameSearch", name);
+		return "admin/Discipline";
+	}
+
+	@GetMapping(value = "admin/addDiscipline")
+	public String addContract(ModelMap model) {
+		List<Employee> employees = employeeService.getAlls();
+		model.addAttribute("employee", employees);
+		return "admin/AddDiscipline";
+	}
+
+	@PostMapping(value = "admin/addDiscipline")
+	public String insertContract(RedirectAttributes redirectAttributes, ModelMap model,
+			@RequestParam("typeDiscipline") String typeDiscipline, @RequestParam("date") String date,
+			@RequestParam("descent") String descent, @RequestParam("reason") String reason,
+			@RequestParam("employeeId") int idEmployee) {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Employee employee = new Employee();
+			Discipline discipline = new Discipline();
+			if (Strings.isEmpty(date)) {
+				List<Employee> employees = employeeService.getAlls();
+				model.addAttribute("idEmployee", idEmployee);
+				model.addAttribute("employee", employees);
+				model.addAttribute("errorDate", "Ngày kỷ luật không được để trống");
+				return "admin/AddDiscipline";
+			}
+			if (Strings.isEmpty(reason)) {
+				List<Employee> employees = employeeService.getAlls();
+				model.addAttribute("idEmployee", idEmployee);
+				model.addAttribute("employee", employees);
+				model.addAttribute("errorReason", "Lý do kỷ luật không được để trống");
+				return "admin/AddDiscipline";
+			}
+			discipline.setDate(sdf.parse(date));
+			discipline.setDescent(descent);
+			discipline.setReason(reason);
+			employee.setId(idEmployee);
+			discipline.setEmployee(employee);
+			discipline.setType(typeDiscipline);
+			disciplineService.insert(discipline);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return "redirect:/admin/listDiscipline";
+	}
+
+	@GetMapping(value = "admin/updateDiscipline/{id}")
+	public String updateContract(ModelMap model, @PathVariable("id") int id) {
+		Discipline discipline = disciplineService.getDisciplineById(id);
+		model.addAttribute("discipline", discipline);
+		return "admin/EditDiscipline";
+	}
+
+	@PostMapping(value = "admin/updateDiscipline")
+	public String postUpdateContract(RedirectAttributes redirectAttributes, ModelMap model, @RequestParam("id") int id,
+			@RequestParam("typeDiscipline") String typeDiscipline, @RequestParam("date") String date,
+			@RequestParam("descent") String descent, @RequestParam("reason") String reason,
+			@RequestParam("employee") int idEmployee) throws ParseException {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Employee employee = new Employee();
+			Discipline discipline = new Discipline();
+			if (Strings.isEmpty(date)) {
+				List<Employee> employees = employeeService.getAlls();
+				Discipline discipline1 = disciplineService.getDisciplineById(id);
+				discipline1.setDate(null);
+				model.addAttribute("discipline", discipline1);
+				model.addAttribute("employee", employees);
+				model.addAttribute("errorDate", "Ngày kỷ luật không được để trống");
+				return "admin/EditDiscipline";
+			}
+			if (Strings.isEmpty(reason)) {
+				List<Employee> employees = employeeService.getAlls();
+				Discipline discipline1 = disciplineService.getDisciplineById(id);
+				discipline1.setReason(null);
+				model.addAttribute("discipline", discipline1);
+				model.addAttribute("employee", employees);
+				model.addAttribute("errorReason", "Lý do kỷ luật không được để trống");
+				return "admin/EditDiscipline";
+			}
+			employee.setId(idEmployee);
+			discipline.setId(id);
+			discipline.setType(typeDiscipline);
+			discipline.setEmployee(employee);
+			discipline.setReason(reason);
+			discipline.setDescent(descent);
+			discipline.setDate(sdf.parse(date));
+			disciplineService.update(discipline);
+		} catch (ParseException pe) {
+			pe.printStackTrace();
+		}
+		return "redirect:/admin/listDiscipline";
+	}
+
+	@GetMapping(value = "admin/deleteDiscipline/{id}")
+	public String deleteDiscipline(ModelMap model, @PathVariable("id") int id) {
+		disciplineService.delete(id);
+		return "redirect:/admin/listDiscipline";
+	}
 }
