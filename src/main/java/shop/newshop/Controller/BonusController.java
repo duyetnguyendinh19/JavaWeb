5989fcb6fc483ae144458504fa8846e50630a718
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-08-02 21:15:46 +0700

    Update BonusController.java

--- src/main/java/shop/newshop/Controller/BonusController.java
+++ src/main/java/shop/newshop/Controller/BonusController.java
@@ -25,82 +25,91 @@ public class BonusController {
 
 	private String name;
 
-
 	@PostMapping(value = "admin/listBonus")
 	public String bonusListSearch(ModelMap model, @RequestParam("nameEmployee") String nameSearch) {
 		name = nameSearch;
 		model.put("error", "");
-		model.put("bonusList", bonusService.getLimit(0,5,nameSearch));
+		model.put("bonusList", bonusService.getLimit(0, 5, nameSearch));
 		long countAll = bonusService.countAll(nameSearch);
 		long totalPage = 0;
 
-		if(countAll%5==0) {
-			totalPage = countAll/5;
-		}else {
-			totalPage = countAll/5 + 1;
+		if (countAll == 0) {
+			totalPage = 1;
+		} else {
+			if (countAll % 5 == 0) {
+				totalPage = countAll / 5;
+			} else {
+				totalPage = countAll / 5 + 1;
+			}
 		}
 
 		model.put("totalPage", totalPage);
 		model.put("totalBonus", countAll);
 		model.put("firstBonus", 1);
-		if(countAll<5) {
+		if (countAll < 5) {
 			model.put("lastBonus", countAll);
-		}else {
+		} else {
 			model.put("lastBonus", 5);
 		}
 		model.put("nameSearch", nameSearch);
-
 
 		return "admin/Bonus";
 	}
 
 	@GetMapping(value = "admin/listBonus")
-	public String  bonusList(ModelMap model) {
+	public String bonusList(ModelMap model) {
 		model.put("error", "");
-		model.put("bonusList", bonusService.getLimit(0,5,null));
+		model.put("bonusList", bonusService.getLimit(0, 5, null));
 		long countAll = bonusService.countAll(null);
 		long totalPage = 0;
 
-		if(countAll%5==0) {
-			totalPage = countAll/5;
-		}else {
-			totalPage = countAll/5 + 1;
+		if (countAll == 0) {
+			totalPage = 1;
+		} else {
+			if (countAll % 5 == 0) {
+				totalPage = countAll / 5;
+			} else {
+				totalPage = countAll / 5 + 1;
+			}
 		}
 
 		model.put("totalPage", totalPage);
 		model.put("totalBonus", countAll);
 		model.put("firstBonus", 1);
-		if(countAll<5) {
+		if (countAll < 5) {
 			model.put("lastBonus", countAll);
-		}else {
+		} else {
 			model.put("lastBonus", 5);
 		}
 		model.put("nameSearch", null);
-
 
 		return "admin/Bonus";
 	}
 
 	@GetMapping(value = "admin/listBonus/{page}")
-	public String  bonusListPage(ModelMap model,@PathVariable("page") int page) {
+	public String bonusListPage(ModelMap model, @PathVariable("page") int page) {
 		model.put("error", "");
-		model.put("bonusList", bonusService.getLimit((page-1)*5, 5, name));
+		model.put("bonusList", bonusService.getLimit((page - 1) * 5, 5, name));
 
 		long countAll = bonusService.countAll(name);
 		long totalPage = 0;
 
-		if(countAll%5==0) {
-			totalPage = countAll/5;
-		}else {
-			totalPage = countAll/5 + 1;
+		if (countAll == 0) {
+			totalPage = 1;
+		} else {
+			if (countAll % 5 == 0) {
+				totalPage = countAll / 5;
+			} else {
+				totalPage = countAll / 5 + 1;
+			}
 		}
 
 		model.put("totalPage", totalPage);
 		model.put("totalBonus", countAll);
-		model.put("firstBonus", (page-1)*5+1);
-		if(page<totalPage) {
-			model.put("lastBonus", (page-1)*5+5);
-		}else {
+		model.put("firstBonus", (page - 1) * 5 + 1);
+		if (page < totalPage) {
+			model.put("lastBonus", (page - 1) * 5 + 5);
+		} else {
 			model.put("lastBonus", countAll);
 		}
 
@@ -116,21 +125,22 @@ public class BonusController {
 	}
 
 	@PostMapping(value = "admin/addBonus")
-	public String insertContract(RedirectAttributes redirectAttributes, ModelMap model, @RequestParam("typeBonus") String typeBonus,
-			@RequestParam("date") String date, @RequestParam("descent") String descent,@RequestParam("reason") String reason,
+	public String insertContract(RedirectAttributes redirectAttributes, ModelMap model,
+			@RequestParam("typeBonus") String typeBonus, @RequestParam("date") String date,
+			@RequestParam("descent") String descent, @RequestParam("reason") String reason,
 			@RequestParam("employeeId") int idEmployee) {
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 			Employee employee = new Employee();
 			Bonus bonus = new Bonus();
-			if(Strings.isEmpty(typeBonus)){
+			if (Strings.isEmpty(typeBonus)) {
 				List<Employee> employees = employeeService.getAlls();
 				model.addAttribute("idEmployee", idEmployee);
 				model.addAttribute("employee", employees);
 				model.addAttribute("errorType", "Vui lòng chọn kiểu khen thưởng");
 				return "admin/AddBonus";
 			}
-			if(Strings.isEmpty(date)){
+			if (Strings.isEmpty(date)) {
 				List<Employee> employees = employeeService.getAlls();
 				model.addAttribute("idEmployee", idEmployee);
 				model.addAttribute("typeBonus", typeBonus);
@@ -138,7 +148,7 @@ public class BonusController {
 				model.addAttribute("errorDate", "Ngày khen thưởng không được để trống");
 				return "admin/AddBonus";
 			}
-			if(Strings.isEmpty(reason)){
+			if (Strings.isEmpty(reason)) {
 				List<Employee> employees = employeeService.getAlls();
 				model.addAttribute("idEmployee", idEmployee);
 				model.addAttribute("typeBonus", typeBonus);
@@ -167,21 +177,22 @@ public class BonusController {
 	}
 
 	@PostMapping(value = "admin/updateBonus")
-	public String postUpdateContract(RedirectAttributes redirectAttributes, ModelMap model, @RequestParam("typeBonus") String typeBonus,
-			@RequestParam("date") String date, @RequestParam("descent") String descent,@RequestParam("reason") String reason,
+	public String postUpdateContract(RedirectAttributes redirectAttributes, ModelMap model,
+			@RequestParam("typeBonus") String typeBonus, @RequestParam("date") String date,
+			@RequestParam("descent") String descent, @RequestParam("reason") String reason,
 			@RequestParam("employee") int idEmployee, @RequestParam("id") int id) {
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 			Employee employee = new Employee();
 			Bonus bonus = new Bonus();
-			if(Strings.isEmpty(typeBonus)){
+			if (Strings.isEmpty(typeBonus)) {
 				bonus = bonusService.getBonusById(id);
 				model.addAttribute("bonus", bonus);
 				model.addAttribute("idEmployee", idEmployee);
 				model.addAttribute("errorType", "Vui lòng chọn kiểu khen thưởng");
 				return "admin/EditBonus";
 			}
-			if(Strings.isEmpty(date)){
+			if (Strings.isEmpty(date)) {
 				bonus = bonusService.getBonusById(id);
 				bonus.setDate(null);
 				model.addAttribute("bonus", bonus);
@@ -190,7 +201,7 @@ public class BonusController {
 				model.addAttribute("errorDate", "Ngày khen thưởng không được để trống");
 				return "admin/EditBonus";
 			}
-			if(Strings.isEmpty(reason)){
+			if (Strings.isEmpty(reason)) {
 				bonus = bonusService.getBonusById(id);
 				bonus.setReason(null);
 				model.addAttribute("bonus", bonus);
@@ -207,7 +218,7 @@ public class BonusController {
 			bonus.setEmployee(employee);
 			bonus.setType(typeBonus);
 			bonusService.update(bonus);
-		}catch (ParseException pe){
+		} catch (ParseException pe) {
 			pe.printStackTrace();
 		}
 		return "redirect:/admin/listBonus";
