84a09b3715bbac619932efafcad5c1626ea40f92
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-08-02 10:52:26 +0700

    Update EmployeeServiceImpl.java

--- src/main/java/shop/newshop/ServiceImpl/EmployeeServiceImpl.java
+++ src/main/java/shop/newshop/ServiceImpl/EmployeeServiceImpl.java
@@ -17,33 +17,44 @@ public class EmployeeServiceImpl implements EmployeeService{
 	private EmployeeDao empDao;
 	
 	@Override
-	public List<Employee> getAlls() {
-		
+	public List<Employee> getAlls() {	
 		return empDao.getAlls();
 	}
 
 	@Override
 	public boolean insert(Employee emp,Account account) {
-		
 		return empDao.insert(emp, account);
 	}
 
 	@Override
-	public boolean update(Employee emp) {
-		
+	public boolean update(Employee emp) {		
 		return empDao.update(emp);
 	}
 
 	@Override
-	public boolean delete(int idEmployee) {
-		
+	public boolean delete(int idEmployee) {		
 		return empDao.delete(idEmployee);
 	}
 
 	@Override
-	public Employee getEmployeeById(int idEmployee) {
-		
+	public Employee getEmployeeById(int idEmployee) {		
 		return empDao.getEmployeeById(idEmployee);
+	}
+
+	@Override
+	public List<Employee> getLimit(int num, int row, String name) {
+		if(name!=null) {
+			return empDao.getLimit(num, row, name.toLowerCase());
+		}
+		return empDao.getLimit(num, row, name);
+	}
+
+	@Override
+	public long countAll(String name) {
+		if(name!=null) {
+			return empDao.countAll(name.toLowerCase());
+		}
+		return empDao.countAll(name);
 	}
 
 }
