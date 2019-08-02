cb049217d1b89a4f78c0400db6e1c1506d7048cf
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-08-02 21:15:43 +0700

    Update AccountController.java

--- src/main/java/shop/newshop/Controller/AccountController.java
+++ src/main/java/shop/newshop/Controller/AccountController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import shop.newshop.Service.AccountService;
 
-
-
 @Controller
 public class AccountController {
 
@@ -20,82 +18,91 @@ public class AccountController {
 
 	private String name;
 
-
 	@PostMapping(value = "admin/listAccount")
 	public String listAccountSearch(ModelMap model, @RequestParam("nameAccount") String nameSearch) {
 		name = nameSearch;
 		model.put("error", "");
-		model.put("listAccount", accountService.getLimit(0,5,nameSearch));
+		model.put("listAccount", accountService.getLimit(0, 5, nameSearch));
 		long countAll = accountService.countAll(nameSearch);
 		long totalPage = 0;
 
-		if(countAll%5==0) {
-			totalPage = countAll/5;
-		}else {
-			totalPage = countAll/5 + 1;
+		if (countAll == 0) {
+			totalPage = 1;
+		} else {
+			if (countAll % 5 == 0) {
+				totalPage = countAll / 5;
+			} else {
+				totalPage = countAll / 5 + 1;
+			}
 		}
 
 		model.put("totalPage", totalPage);
 		model.put("totalAccount", countAll);
 		model.put("firstAccount", 1);
-		if(countAll<5) {
+		if (countAll < 5) {
 			model.put("lastAccount", countAll);
-		}else {
+		} else {
 			model.put("lastAccount", 5);
 		}
 		model.put("nameSearch", nameSearch);
-
 
 		return "admin/user";
 	}
 
 	@GetMapping(value = "admin/listAccount")
-	public String  listAccount(ModelMap model) {
+	public String listAccount(ModelMap model) {
 		model.put("error", "");
-		model.put("listAccount", accountService.getLimit(0,5,null));
+		model.put("listAccount", accountService.getLimit(0, 5, null));
 		long countAll = accountService.countAll(null);
 		long totalPage = 0;
 
-		if(countAll%5==0) {
-			totalPage = countAll/5;
-		}else {
-			totalPage = countAll/5 + 1;
+		if (countAll == 0) {
+			totalPage = 1;
+		} else {
+			if (countAll % 5 == 0) {
+				totalPage = countAll / 5;
+			} else {
+				totalPage = countAll / 5 + 1;
+			}
 		}
 
 		model.put("totalPage", totalPage);
 		model.put("totalAccount", countAll);
 		model.put("firstAccount", 1);
-		if(countAll<5) {
+		if (countAll < 5) {
 			model.put("lastAccount", countAll);
-		}else {
+		} else {
 			model.put("lastAccount", 5);
 		}
 		model.put("nameSearch", null);
-
 
 		return "admin/user";
 	}
 
 	@GetMapping(value = "admin/listAccount/{page}")
-	public String  listAccountPage(ModelMap model,@PathVariable("page") int page) {
+	public String listAccountPage(ModelMap model, @PathVariable("page") int page) {
 		model.put("error", "");
-		model.put("listAccount", accountService.getLimit((page-1)*5, 5, name));
+		model.put("listAccount", accountService.getLimit((page - 1) * 5, 5, name));
 
 		long countAll = accountService.countAll(name);
 		long totalPage = 0;
 
-		if(countAll%5==0) {
-			totalPage = countAll/5;
-		}else {
-			totalPage = countAll/5 + 1;
+		if (countAll == 0) {
+			totalPage = 1;
+		} else {
+			if (countAll % 5 == 0) {
+				totalPage = countAll / 5;
+			} else {
+				totalPage = countAll / 5 + 1;
+			}
 		}
 
 		model.put("totalPage", totalPage);
 		model.put("totalAccount", countAll);
-		model.put("firstAccount", (page-1)*5+1);
-		if(page<totalPage) {
-			model.put("lastAccount", (page-1)*5+5);
-		}else {
+		model.put("firstAccount", (page - 1) * 5 + 1);
+		if (page < totalPage) {
+			model.put("lastAccount", (page - 1) * 5 + 5);
+		} else {
 			model.put("lastAccount", countAll);
 		}
 
