b09044714ddcf8c4a09ed5a01b247a744f561887
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-08-02 12:53:27 +0700

    Update AccountController.java

--- src/main/java/shop/newshop/Controller/AccountController.java
+++ src/main/java/shop/newshop/Controller/AccountController.java
@@ -4,38 +4,102 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestParam;
-import shop.newshop.Entity.Account;
 import shop.newshop.Service.AccountService;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Controller
-@RequestMapping(value = "admin")
 public class AccountController {
 
-    @Autowired
-    private AccountService accountService;
+	@Autowired
+	private AccountService accountService;
 
-    @GetMapping("listAccount")
-    public String listAccount(ModelMap model){
-        List<Account> accountList = accountService.getAlls();
-        model.addAttribute("listAccount",accountList);
-        return "admin/user";
-    }
+	private String name;
 
-    @RequestMapping(value = "listAccount", method = RequestMethod.POST)
-    public String getBonusPost(ModelMap map , @RequestParam("user") String user) {
-        List<Account> accountList = new ArrayList<>();
-        accountList = accountService.searchByUser(user);
-        if(accountList.size() == 0){
-            map.addAttribute("searchFail", "Không tìm thấy tài khoản là: " + user);
-        }
-        map.addAttribute("nameSearch" ,user);
-        map.put("listAccount", accountList);
-        return "admin/user";
-    }
+
+	@PostMapping(value = "admin/listAccount")
+	public String listAccountSearch(ModelMap model, @RequestParam("nameAccount") String nameSearch) {
+		name = nameSearch;
+		model.put("error", "");
+		model.put("listAccount", accountService.getLimit(0,5,nameSearch));
+		long countAll = accountService.countAll(nameSearch);
+		long totalPage = 0;
+
+		if(countAll%5==0) {
+			totalPage = countAll/5;
+		}else {
+			totalPage = countAll/5 + 1;
+		}
+
+		model.put("totalPage", totalPage);
+		model.put("totalAccount", countAll);
+		model.put("firstAccount", 1);
+		if(countAll<5) {
+			model.put("lastAccount", countAll);
+		}else {
+			model.put("lastAccount", 5);
+		}
+		model.put("nameSearch", nameSearch);
+
+
+		return "admin/user";
+	}
+
+	@GetMapping(value = "admin/listAccount")
+	public String  listAccount(ModelMap model) {
+		model.put("error", "");
+		model.put("listAccount", accountService.getLimit(0,5,null));
+		long countAll = accountService.countAll(null);
+		long totalPage = 0;
+
+		if(countAll%5==0) {
+			totalPage = countAll/5;
+		}else {
+			totalPage = countAll/5 + 1;
+		}
+
+		model.put("totalPage", totalPage);
+		model.put("totalAccount", countAll);
+		model.put("firstAccount", 1);
+		if(countAll<5) {
+			model.put("lastAccount", countAll);
+		}else {
+			model.put("lastAccount", 5);
+		}
+		model.put("nameSearch", null);
+
+
+		return "admin/user";
+	}
+
+	@GetMapping(value = "admin/listAccount/{page}")
+	public String  listAccountPage(ModelMap model,@PathVariable("page") int page) {
+		model.put("error", "");
+		model.put("listAccount", accountService.getLimit((page-1)*5, 5, name));
+
+		long countAll = accountService.countAll(name);
+		long totalPage = 0;
+
+		if(countAll%5==0) {
+			totalPage = countAll/5;
+		}else {
+			totalPage = countAll/5 + 1;
+		}
+
+		model.put("totalPage", totalPage);
+		model.put("totalAccount", countAll);
+		model.put("firstAccount", (page-1)*5+1);
+		if(page<totalPage) {
+			model.put("lastAccount", (page-1)*5+5);
+		}else {
+			model.put("lastAccount", countAll);
+		}
+
+		model.put("nameSearch", name);
+		return "admin/user";
+	}
 }
