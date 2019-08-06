2b000933ea5a8bb3f581fb1caf33f6f53139009d
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-08-06 15:09:42 +0700

    Update checkDB.java

--- src/main/java/checkDB.java
+++ src/main/java/checkDB.java
@@ -4,6 +4,11 @@ import shop.newshop.util.HibernateUtils;
 
 public class checkDB {
 	public static void main(String[] args) {
-		Session sesion = HibernateUtils.getSessionFactory().openSession();
+//		Session sesion = HibernateUtils.getSessionFactory().openSession();
+		String a = "a b c";
+		String b = "c";
+		if(a.toLowerCase().trim().contains(b.toLowerCase().trim())) {
+			System.out.println("hi");
+		}
 	}
 }
