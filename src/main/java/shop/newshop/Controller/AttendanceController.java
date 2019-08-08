80642b1a9378292e61d398c2ed4e19ce8736d37c
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-08-08 16:27:35 +0700

    Update AttendanceController.java

--- src/main/java/shop/newshop/Controller/AttendanceController.java
+++ src/main/java/shop/newshop/Controller/AttendanceController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,51 +28,178 @@ import java.util.List;
 @RequestMapping(value = "admin")
 public class AttendanceController {
 
-    @Autowired
-    private AttendanceService attendanceService;
+	@Autowired
+	private AttendanceService attendanceService;
 
-    @Autowired
-    private EmployeeService employeeService;
+	@Autowired
+	private EmployeeService employeeService;
 
-    @Autowired
-    private TotalAttendanceService totalAttendanceService;
+	@Autowired
+	private TotalAttendanceService totalAttendanceService;
 
-    @GetMapping(value = "listAttendance")
-    public String getAttendance(ModelMap model) {
-        Date date = new Date();
-        List<Attendance> attendanceList = attendanceService.getAll();
-        model.addAttribute("listAttendance", attendanceList);
-        List<Employee> employeeList = employeeService.getAlls();
-        List<Object[]> object = totalAttendanceService.listTotal(date.getMonth() + 1,1900 + date.getYear());
-        List<TotalAttendance> listToTal = new ArrayList<>();
-        for(Object[] x : object){
-            TotalAttendance totalAttendance = new TotalAttendance();
-            totalAttendance.setIdEmployee(Integer.parseInt(String.valueOf(x[0])));
-            totalAttendance.setNameEmployee(String.valueOf(x[1]));
-            totalAttendance.setSalary(Float.parseFloat(String.valueOf(x[2])));
-            totalAttendance.setMonth(Integer.parseInt(String.valueOf(x[3])));
-            totalAttendance.setCount(Double.parseDouble(String.valueOf(x[4])));
-            totalAttendance.setDay_off(Double.parseDouble(String.valueOf(x[5])));
-            totalAttendance.setTotalSalary(Double.parseDouble((String.valueOf(x[6]))));
-            listToTal.add(totalAttendance);
-        }
-        model.addAttribute("listTotal", listToTal);
-        return "admin/Attendance";
-    }
+	String nameSearch;
 
-    @GetMapping(value = "addAttendance")
-    public String addAttendance(ModelMap model){
-        List<Employee> employees = employeeService.getAlls();
-        model.addAttribute("employee", employees);
+	String dateSearch;
 
-        return "admin/AddAttendance";
-    }
+	@PostMapping(value = "listAttendance")
+	public String getAttendanceSearch(ModelMap model, @RequestParam("name") String name,
+			@RequestParam("date") String dateS) {
+		Date date = new Date();
+		nameSearch = name;
+		dateSearch = dateS;
+		model.put("name", name);
 
-    @PostMapping(value = "addAttendance")
-    public String postAttendance(ModelMap model, @RequestParam("date") String dateInput,
-                                 @RequestParam("employee") int employeeId, @RequestParam("timestart") String timestart,
-                                 @RequestParam("timeend") String timeend) {
-        try {
+		model.put("date", dateS);
+
+		List<Attendance> attendanceList = attendanceService.getLimit(0, 10, name, dateS);
+		model.addAttribute("listAttendance", attendanceList);
+		List<Employee> employeeList = employeeService.getAlls();
+		List<Object[]> object = totalAttendanceService.listTotal(date.getMonth() + 1, 1900 + date.getYear());
+		List<TotalAttendance> listToTal = new ArrayList<>();
+		for (Object[] x : object) {
+			TotalAttendance totalAttendance = new TotalAttendance();
+			totalAttendance.setIdEmployee(Integer.parseInt(String.valueOf(x[0])));
+			totalAttendance.setNameEmployee(String.valueOf(x[1]));
+			totalAttendance.setSalary(Float.parseFloat(String.valueOf(x[2])));
+			totalAttendance.setMonth(Integer.parseInt(String.valueOf(x[3])));
+			totalAttendance.setCount(Double.parseDouble(String.valueOf(x[4])));
+			totalAttendance.setDay_off(Double.parseDouble(String.valueOf(x[5])));
+			totalAttendance.setTotalSalary(Double.parseDouble((String.valueOf(x[6]))));
+			listToTal.add(totalAttendance);
+		}
+		model.addAttribute("listTotal", listToTal);
+		long countAll = attendanceService.countAll(name, dateS);
+		long totalPage = 0;
+
+		if (countAll == 0) {
+			totalPage = 1;
+			model.addAttribute("searchFail", "Không tìm thấy dữ liệu");
+		} else {
+			if (countAll % 10 == 0) {
+				totalPage = countAll / 10;
+			} else {
+				totalPage = countAll / 10 + 1;
+			}
+		}
+
+		model.put("totalPage", totalPage);
+		model.put("totalAtten", countAll);
+		model.put("firstAtten", 1);
+		if (countAll < 5) {
+			model.put("lastAtten", countAll);
+		} else {
+			model.put("lastAtten", 10);
+		}
+		return "admin/Attendance";
+	}
+
+	@GetMapping(value = "listAttendance")
+	public String getAttendance(ModelMap model) {
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		model.put("date", sdf.format(date));
+		SimpleDateFormat sdff = new SimpleDateFormat("yyyy/MM/dd");
+		List<Attendance> attendanceList = attendanceService.getLimit(0, 10, null, sdff.format(date));
+
+		model.addAttribute("listAttendance", attendanceList);
+		List<Employee> employeeList = employeeService.getAlls();
+		List<Object[]> object = totalAttendanceService.listTotal(date.getMonth() + 1, 1900 + date.getYear());
+		List<TotalAttendance> listToTal = new ArrayList<>();
+		for (Object[] x : object) {
+			TotalAttendance totalAttendance = new TotalAttendance();
+			totalAttendance.setIdEmployee(Integer.parseInt(String.valueOf(x[0])));
+			totalAttendance.setNameEmployee(String.valueOf(x[1]));
+			totalAttendance.setSalary(Float.parseFloat(String.valueOf(x[2])));
+			totalAttendance.setMonth(Integer.parseInt(String.valueOf(x[3])));
+			totalAttendance.setCount(Double.parseDouble(String.valueOf(x[4])));
+			totalAttendance.setDay_off(Double.parseDouble(String.valueOf(x[5])));
+			totalAttendance.setTotalSalary(Double.parseDouble((String.valueOf(x[6]))));
+			listToTal.add(totalAttendance);
+		}
+		model.addAttribute("listTotal", listToTal);
+		long countAll = attendanceService.countAll(null, sdff.format(date));
+		long totalPage = 0;
+
+		if (countAll == 0) {
+			totalPage = 1;
+			model.addAttribute("searchFail", "Không tìm thấy dữ liệu");
+		} else {
+			if (countAll % 10 == 0) {
+				totalPage = countAll / 10;
+			} else {
+				totalPage = countAll / 10 + 1;
+			}
+		}
+
+		model.put("totalPage", totalPage);
+		model.put("totalAtten", countAll);
+		model.put("firstAtten", 1);
+		if (countAll < 5) {
+			model.put("lastAtten", countAll);
+		} else {
+			model.put("lastAtten", 10);
+		}
+		return "admin/Attendance";
+	}
+
+	@GetMapping(value = "listAttendance/{page}")
+	public String getAttendancePage(ModelMap model, @PathVariable("page") int page) {
+		Date date = new Date();
+		List<Attendance> attendanceList = attendanceService.getLimit((page - 1) * 10, 10, nameSearch, dateSearch);
+		model.addAttribute("listAttendance", attendanceList);
+		List<Employee> employeeList = employeeService.getAlls();
+		List<Object[]> object = totalAttendanceService.listTotal(date.getMonth() + 1, 1900 + date.getYear());
+		List<TotalAttendance> listToTal = new ArrayList<>();
+		for (Object[] x : object) {
+			TotalAttendance totalAttendance = new TotalAttendance();
+			totalAttendance.setIdEmployee(Integer.parseInt(String.valueOf(x[0])));
+			totalAttendance.setNameEmployee(String.valueOf(x[1]));
+			totalAttendance.setSalary(Float.parseFloat(String.valueOf(x[2])));
+			totalAttendance.setMonth(Integer.parseInt(String.valueOf(x[3])));
+			totalAttendance.setCount(Double.parseDouble(String.valueOf(x[4])));
+			totalAttendance.setDay_off(Double.parseDouble(String.valueOf(x[5])));
+			totalAttendance.setTotalSalary(Double.parseDouble((String.valueOf(x[6]))));
+			listToTal.add(totalAttendance);
+		}
+		model.addAttribute("listTotal", listToTal);
+		long countAll = attendanceService.countAll(nameSearch, dateSearch);
+		long totalPage = 0;
+
+		if (countAll == 0) {
+			totalPage = 1;
+			model.addAttribute("searchFail", "Không tìm thấy dữ liệu");
+		} else {
+			if (countAll % 10 == 0) {
+				totalPage = countAll / 10;
+			} else {
+				totalPage = countAll / 10 + 1;
+			}
+		}
+
+		model.put("totalPage", totalPage);
+		model.put("totalAtten", countAll);
+		model.put("firstAtten", (page - 1) * 10 + 1);
+		if (countAll < totalPage) {
+			model.put("lastAtten", (page - 1) * 10 + 10);
+		} else {
+			model.put("lastAtten", countAll);
+		}
+		return "admin/Attendance";
+	}
+
+	@GetMapping(value = "addAttendance")
+	public String addAttendance(ModelMap model) {
+		List<Employee> employees = employeeService.getAlls();
+		model.addAttribute("employee", employees);
+
+		return "admin/AddAttendance";
+	}
+
+	@PostMapping(value = "addAttendance")
+	public String postAttendance(ModelMap model, @RequestParam("date") String dateInput,
+			@RequestParam("employee") int employeeId, @RequestParam("timestart") String timestart,
+			@RequestParam("timeend") String timeend) {
+		try {
 //            Calendar c = Calendar.getInstance();
 //            Date date = sdf.parse(startDate);
 //            c.setTime(date);
@@ -81,55 +209,55 @@ public class AttendanceController {
 //            Lấy time hiện tại
 //            LocalTime time = LocalTime.now();
 //            System.out.println("Time: " + time.getHour() + ":" + time.getMinute() + ":" + time.getSecond());
-            if (timestart.compareTo(timeend) > 0) {
-                List<Employee> employees = employeeService.getAlls();
-                model.addAttribute("employee", employees);
-                model.addAttribute("idEmployee", employeeId);
-                model.addAttribute("startTime", " Thời gian kết thúc phải lớn hơn thời gian bắt đầu");
-                return "admin/AddAttendance";
-            }
-            if (Strings.isEmpty(dateInput)) {
-                List<Employee> employees = employeeService.getAlls();
-                model.addAttribute("employee", employees);
-                model.addAttribute("idEmployee", employeeId);
-                model.addAttribute("errorDate", " Ngày không được để trống");
-                return "admin/AddAttendance";
-            }
-            Format sdf = new SimpleDateFormat("dd/MM/yyyy");
-            Date date = (Date) sdf.parseObject(dateInput);
-            Attendance attendance = new Attendance();
-            Employee employee = new Employee();
-            employee.setId(employeeId);
-            System.out.println(employeeId);
-            System.out.println(dateInput);
-            List<Attendance> list = attendanceService.getAttenDanceByDateAndId(employeeId, date);
-            if (list.size() > 0) {
-                if (list.get(0).getEndtime().compareTo(Time.valueOf(timeend)) > 0) {
-                    List<Employee> employees = employeeService.getAlls();
-                    model.addAttribute("employee", employees);
-                    model.addAttribute("idEmployee", employeeId);
-                    model.addAttribute("startTime", "Thời gian kết thúc phải lớn hơn thời gian kết thức lần trước");
-                    return "admin/AddAttendance";
-                } else {
-                    attendance.setId(list.get(0).getId());
-                    attendance.setMonth(date.getMonth() + 1);
-                    attendance.setEmployee(employee);
-                    attendance.setStarttime(list.get(0).getStarttime());
-                    attendance.setDate(date);
-                    attendance.setEndtime(Time.valueOf(timeend));
-                    attendanceService.update(attendance);
-                }
-            } else {
-                attendance.setDate(date);
-                attendance.setEndtime(Time.valueOf(timeend));
-                attendance.setStarttime(Time.valueOf(timestart));
-                attendance.setEmployee(employee);
-                attendance.setMonth(date.getMonth() + 1);
-                attendanceService.insert(attendance);
-            }
-        } catch (ParseException pe) {
-            pe.printStackTrace();
-        }
-        return "redirect:/admin/listAttendance";
-    }
+			if (timestart.compareTo(timeend) > 0) {
+				List<Employee> employees = employeeService.getAlls();
+				model.addAttribute("employee", employees);
+				model.addAttribute("idEmployee", employeeId);
+				model.addAttribute("startTime", " Thời gian kết thúc phải lớn hơn thời gian bắt đầu");
+				return "admin/AddAttendance";
+			}
+			if (Strings.isEmpty(dateInput)) {
+				List<Employee> employees = employeeService.getAlls();
+				model.addAttribute("employee", employees);
+				model.addAttribute("idEmployee", employeeId);
+				model.addAttribute("errorDate", " Ngày không được để trống");
+				return "admin/AddAttendance";
+			}
+			Format sdf = new SimpleDateFormat("dd/MM/yyyy");
+			Date date = (Date) sdf.parseObject(dateInput);
+			Attendance attendance = new Attendance();
+			Employee employee = new Employee();
+			employee.setId(employeeId);
+			System.out.println(employeeId);
+			System.out.println(dateInput);
+			List<Attendance> list = attendanceService.getAttenDanceByDateAndId(employeeId, date);
+			if (list.size() > 0) {
+				if (list.get(0).getEndtime().compareTo(Time.valueOf(timeend)) > 0) {
+					List<Employee> employees = employeeService.getAlls();
+					model.addAttribute("employee", employees);
+					model.addAttribute("idEmployee", employeeId);
+					model.addAttribute("startTime", "Thời gian kết thúc phải lớn hơn thời gian kết thức lần trước");
+					return "admin/AddAttendance";
+				} else {
+					attendance.setId(list.get(0).getId());
+					attendance.setMonth(date.getMonth() + 1);
+					attendance.setEmployee(employee);
+					attendance.setStarttime(list.get(0).getStarttime());
+					attendance.setDate(date);
+					attendance.setEndtime(Time.valueOf(timeend));
+					attendanceService.update(attendance);
+				}
+			} else {
+				attendance.setDate(date);
+				attendance.setEndtime(Time.valueOf(timeend));
+				attendance.setStarttime(Time.valueOf(timestart));
+				attendance.setEmployee(employee);
+				attendance.setMonth(date.getMonth() + 1);
+				attendanceService.insert(attendance);
+			}
+		} catch (ParseException pe) {
+			pe.printStackTrace();
+		}
+		return "redirect:/admin/listAttendance";
+	}
 }
