db0e79276bf93bb600858639b463c52d9a7c2bf4
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-08-09 09:32:54 +0700

    Update ServiceInterceptor.java

--- src/main/java/shop/newshop/Interceptor/ServiceInterceptor.java
+++ src/main/java/shop/newshop/Interceptor/ServiceInterceptor.java
@@ -2,27 +2,37 @@ package shop.newshop.Interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import shop.newshop.Entity.Account;
 
 @Component
 public class ServiceInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-
+		HttpSession session = request.getSession();
+		if (session.getAttribute("account") == null) {
+			response.sendRedirect(request.getContextPath() + "/");
+			return false;
+		}
 		return true;
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
+		
+
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
 			Exception exception) throws Exception {
+		
 	}
 }
