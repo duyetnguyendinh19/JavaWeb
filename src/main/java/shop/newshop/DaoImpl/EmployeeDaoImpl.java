d22b6ac712bc19113cff1f6ccd957efd5e0df42b
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-08-10 09:43:54 +0700

    Update EmployeeDaoImpl.java

--- src/main/java/shop/newshop/DaoImpl/EmployeeDaoImpl.java
+++ src/main/java/shop/newshop/DaoImpl/EmployeeDaoImpl.java
@@ -14,82 +14,83 @@ import shop.newshop.util.HibernateUtils;
 @Repository
 public class EmployeeDaoImpl implements EmployeeDao {
 
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	@Override
-    public List<Employee> getAlls() {
-        List<Employee> list = null;
-        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
-        try {
-            String sql = "FROM Employee";
-            session.beginTransaction();
-            Query query = session.createQuery(sql);
-            list = query.list();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            session.close();
-        }
-        return list;
-    }
+	public List<Employee> getAlls() {
+		List<Employee> list = null;
+		Session session = HibernateUtils.getSessionFactory().getCurrentSession();
+		try {
+			String sql = "FROM Employee";
+			session.beginTransaction();
+			Query query = session.createQuery(sql);
+			list = query.list();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return list;
+	}
 
-    @Override
-    public boolean insert(Employee emp,Account account) {
-        Session session = HibernateUtils.getSessionFactory().openSession();
-        try {
-            session.beginTransaction();
-            session.save(account);
-            session.flush();
-            emp.setAccount(account);;
-            session.save(emp);
-            session.getTransaction().commit();
-            return true;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            e.printStackTrace();
-        }finally {
-        	session.close();
-        }
-        return false;
-    }
+	@Override
+	public boolean insert(Employee emp, Account account) {
+		Session session = HibernateUtils.getSessionFactory().openSession();
+		try {
+			session.beginTransaction();
+			session.save(account);
+			session.flush();
+			emp.setAccount(account);
+			;
+			session.save(emp);
+			session.getTransaction().commit();
+			return true;
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return false;
+	}
 
-    @Override
-    public boolean update(Employee emp) {
-        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
-        try {
-            session.beginTransaction();
-            session.update(emp);
-            session.getTransaction().commit();
-            return true;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        }
-        return false;
-    }
+	@Override
+	public boolean update(Employee emp) {
+		Session session = HibernateUtils.getSessionFactory().getCurrentSession();
+		try {
+			session.beginTransaction();
+			session.update(emp);
+			session.getTransaction().commit();
+			return true;
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+		}
+		return false;
+	}
 
-    @Override
-    public boolean delete(int idEmployee) {
-        Employee employee = getEmployeeById(idEmployee);
-        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
-        try {
-            session.beginTransaction();
-            session.delete(employee);
-            session.getTransaction().commit();
-            return true;
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-            e.printStackTrace();
-        }
-        return false;
-    }
+	@Override
+	public boolean delete(int idEmployee) {
+		Employee employee = getEmployeeById(idEmployee);
+		Session session = HibernateUtils.getSessionFactory().getCurrentSession();
+		try {
+			session.beginTransaction();
+			session.delete(employee);
+			session.getTransaction().commit();
+			return true;
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			e.printStackTrace();
+		}
+		return false;
+	}
 
-    @Override
-    public Employee getEmployeeById(int idEmployee) {
-        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Employee employee = (Employee) session.get(Employee.class, idEmployee);
-        session.close();
-        return employee;
-    }
+	@Override
+	public Employee getEmployeeById(int idEmployee) {
+		Session session = HibernateUtils.getSessionFactory().getCurrentSession();
+		session.beginTransaction();
+		Employee employee = (Employee) session.get(Employee.class, idEmployee);
+		session.close();
+		return employee;
+	}
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -100,16 +101,16 @@ public class EmployeeDaoImpl implements EmployeeDao {
 			session.beginTransaction();
 			String sql = "FROM Employee Where 1=1";
 			String hqlWhere = " ";
-			if(name!=null && !name.isEmpty()) {
-				hqlWhere += "AND LOWER(name) LIKE '%"+name+"%'";
+			if (name != null && !name.isEmpty()) {
+				hqlWhere += "AND LOWER(name) LIKE '%" + name + "%'";
 			}
-			Query query = session.createQuery(sql+hqlWhere);
+			Query query = session.createQuery(sql + hqlWhere);
 			query.setFirstResult(num);
 			query.setMaxResults(row);
 			list = query.list();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			session.close();
 		}
 		return list;
@@ -123,14 +124,14 @@ public class EmployeeDaoImpl implements EmployeeDao {
 			session.beginTransaction();
 			String hql = "SELECT COUNT(*) FROM Employee Where 1=1";
 			String hqlWhere = " ";
-			if(name!=null && !name.isEmpty()) {
-				hqlWhere += " AND LOWER(name) LIKE '%"+name+"%'";
+			if (name != null && !name.isEmpty()) {
+				hqlWhere += " AND LOWER(name) LIKE '%" + name + "%'";
 			}
-			Query query = session.createQuery(hql+hqlWhere);
+			Query query = session.createQuery(hql + hqlWhere);
 			result = (long) query.uniqueResult();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			session.close();
 		}
 		return result;
@@ -143,15 +144,47 @@ public class EmployeeDaoImpl implements EmployeeDao {
 			String Hql = "FROM Employee WHERE email=:email";
 			Query query = session.createQuery(Hql);
 			query.setParameter("email", email);
-			if(query.list()!=null && !query.list().isEmpty()) {
+			if (query.list() != null && !query.list().isEmpty()) {
 				return true;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			session.close();
 		}
 		return false;
+	}
+
+	@Override
+	public long check(String email, String phone, String username, String card, int id) {
+		long result = 0;
+		Session session = HibernateUtils.getSessionFactory().openSession();
+
+		try {
+
+			if ((long) (session.createQuery("Select Count(*) From Employee Where email=:email AND id!=:id")
+					.setParameter("email", email).setParameter("id", id).uniqueResult()) > 0) {
+				result = 1;
+			} else if ((long) (session.createQuery("Select Count(*) From Employee Where phone=:phone AND id!=:id")
+					.setParameter("phone", phone).setParameter("id", id).uniqueResult()) > 0) {
+				result = 2;
+			} else if ((long) (session
+					.createQuery("Select Count(*) From Employee Where identitycard=:identitycard AND id!=:id")
+					.setParameter("identitycard", card).setParameter("id", id).uniqueResult()) > 0) {
+				result = 3;
+			} else if ((long) (session
+					.createQuery("Select Count(*) From Employee e Join e.account ac Where ac.username=:username AND e.id!=:id")
+					.setParameter("username", username).setParameter("id", id).uniqueResult()) > 0) {
+				result = 4;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+
+		return result;
 	}
 
 }
