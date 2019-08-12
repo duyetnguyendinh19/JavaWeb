460f8dd5d248ddcca6a919a4d5741e405bed7508
duyetnguyendinh19@gmail.com <duyetnguyendinh19@gmail.com>
2019-08-12 11:36:49 +0700

    Update BonusDao.java

--- src/main/java/shop/newshop/DAO/BonusDao.java
+++ src/main/java/shop/newshop/DAO/BonusDao.java
@@ -3,12 +3,15 @@ package shop.newshop.DAO;
 import java.util.List;
 
 import shop.newshop.Entity.Bonus;
+import shop.newshop.Entity.Employee;
 
 public interface BonusDao {
 	
 	public List<Bonus> getAlls();
 
 	public List<Bonus> getLimit(int num, int row, String name);
+	
+	public List<Bonus> getBonusByEmployee(Employee employee);
 
 	public long countAll(String name);
 
