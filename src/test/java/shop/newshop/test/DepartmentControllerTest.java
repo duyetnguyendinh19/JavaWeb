a28dc10a95a7a47847a403e1275df930572b7774
duyetnguyen <46662469+duyetnguyendinh19@users.noreply.github.com>
2020-06-18 18:38:22 +0700

    cm

--- src/test/java/shop/newshop/test/DepartmentControllerTest.java
+++ src/test/java/shop/newshop/test/DepartmentControllerTest.java
@@ -1,11 +1,15 @@
 package shop.newshop.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.nio.charset.Charset;
+import java.util.Random;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -14,20 +18,132 @@ import shop.newshop.Service.DepartmentService;
 import shop.newshop.config.DepartmentConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {DepartmentConfig.class})
+@ContextConfiguration(classes = { DepartmentConfig.class })
 public class DepartmentControllerTest {
 
 	private DepartmentService departmentService;
-	
+	private int departmentId = 2;
+	private String departmentName = "Kiểm duyệt";
+	private int lengthRandomName = 7;
+
 	@Autowired
-    public void setProductService(DepartmentService departmentService){
-        this.departmentService = departmentService;
-    }
+	public void setProductService(DepartmentService departmentService) {
+		this.departmentService = departmentService;
+	}
+
+	@Test
+	public void testGetDepartment() {
+		Department depart = departmentService.getDepartById(departmentId);
+		if (depart == null) {
+			assertFalse(true, "Không tìm thấy phòng ban");
+		} else {
+			assertEquals(departmentName, depart.getName());
+		}
+
+	}
+
+	@Test
+	public void testRequiredInsertDepartName() {
+		if (departmentName == null || departmentName.isEmpty()) {
+			assertFalse(true, "Tên phòng ban không được để trống");
+		} else {
+//			Department depart = new Department();
+//			depart.setName(departmentName);
+//			departmentService.insert(depart);
+			assertTrue(true, "Thêm mới thành công");
+		}
+	}
+
+	@Test
+	public void testExistInsertDepartName() {
+		if (departmentName != null) {
+			if (departmentService.checkName(departmentName, departmentId) > 0) {
+				assertFalse(true, "Tên phòng ban đã tồn tại");
+			} else {
+//				Department depart = new Department();
+//				depart.setName(departmentName);
+//				departmentService.insert(depart);
+				assertTrue(true, "Thêm mới thành công");
+			}
+		} else {
+			assertFalse(true, "Tên phòng ban không được để trống");
+		}
+	}
+
+	@Test
+	public void testExceptionInsert() {
+		try {
+			Department depart = new Department();
+			byte[] array = new byte[lengthRandomName];
+			new Random().nextBytes(array);
+			String departmentNameRandom = new String(array, Charset.forName("UTF-8"));
+			depart.setName(departmentNameRandom);
+			departmentService.insert(depart);
+			assertTrue(true, "Thêm phòng ban thành công");
+		} catch (Exception e) {
+			assertFalse(true, "Xảy ra ngoại lệ: " + e);
+		}
+	}
+
+	@Test
+	public void testRequiredUpdateDepartName() {
+		if (departmentName == null || departmentName.isEmpty()) {
+			assertFalse(true, "Tên phòng ban không được để trống");
+		} else {
+//			Department depart = new Department();
+//			depart.setName(departmentName);
+//			departmentService.update(depart);
+			assertTrue(true, "Sửa phòng ban thành công");
+		}
+	}
+
+	@Test
+	public void testExistUpdateDepartName() {
+		if (departmentName != null) {
+			if (departmentService.checkName(departmentName, departmentId) > 0) {
+				assertFalse(true, "Tên phòng ban đã tồn tại");
+			} else {
+//				Department depart = new Department();
+//				depart.setName(departmentName);
+//				departmentService.update(depart);
+				assertTrue(true, "Sửa phòng ban thành công");
+			}
+		} else {
+			assertFalse(true, "Tên phòng ban không được để trống");
+		}
+	}
+
+	@Test
+	public void testExceptionUpdate() {
+		try {
+			Department depart = departmentService.getDepartById(departmentId);
+			byte[] array = new byte[lengthRandomName];
+			new Random().nextBytes(array);
+			String departmentNameRandom = new String(array, Charset.forName("UTF-8"));
+			depart.setName(departmentNameRandom);
+			departmentService.update(depart);
+			assertTrue(true, "Sửa phòng ban thành công");
+		} catch (Exception e) {
+			assertFalse(true, "Xảy ra ngoại lệ: " + e);
+		}
+	}
+
+	@Test
+	public void testfindDepart() {
+		Department depart = departmentService.getDepartById(departmentId);
+		if (depart == null) {
+			assertFalse(true, "Bản ghi đã bị xóa, vui lòng refresh lại trang");
+		}
+	}
 	
 	@Test
-    public void testGetDepartment(){
-        Department depart = departmentService.getDepartById(1);
-        assertEquals("Nhân sự", depart.getName());
-    }
-	
+	public void testDeleteDepart() {
+		Department depart = departmentService.getDepartById(departmentId);
+		if (depart == null) {
+			assertFalse(true, "Bản ghi đã bị xóa, vui lòng refresh lại trang");
+		}else {
+			assertTrue(true, "Xóa phòng ban thành công");
+		}
+	}
+
 }
